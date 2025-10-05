de7ac10c1e2d1e01cc2dce2a78ef9a72d28e0e0c
Doud75 <adrienquimbre@gmail.com>
2025-10-05 20:27:48 +0200

    fix : state for live score

--- content.js
+++ content.js
@@ -1,53 +1,58 @@
+let observer = null;
 
-function hideSpoilers(enabled) {
-  const selectorsToHide = [
-    '.imspo_mt__t-sc .imspo_mt__tt-w',
-    '.imspo_mt__triangle',
-    '.imso_mh__l-tm-sc',
-    '.imso_mh__r-tm-sc',
-    '.imso_mh__sep'
-  ];
-  const hiddenClassName = 'spoiler-is-hidden';
+const selectorsToHide = [
+  '.imspo_mt__t-sc .imspo_mt__tt-w',
+  '.imspo_mt__triangle',
+  '.imso_mh__l-tm-sc',
+  '.imso_mh__r-tm-sc',
+  '.imso_mh__sep'
+];
+const hiddenClassName = 'spoiler-is-hidden';
 
-  if (!enabled) {
+function applyHidingRules() {
+  selectorsToHide.forEach(selector => {
+    document.querySelectorAll(selector).forEach(element => {
+      if (!element.classList.contains(hiddenClassName)) {
+        element.style.visibility = 'hidden';
+        element.classList.add(hiddenClassName);
+      }
+    });
+  });
+}
+
+function toggleSpoilers(enabled) {
+  if (enabled) {
+    applyHidingRules();
+
+    if (!observer) {
+      observer = new MutationObserver(applyHidingRules);
+    }
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true
+    });
+
+  } else {
+    if (observer) {
+      observer.disconnect();
+      observer = null;
+    }
+
     document.querySelectorAll(`.${hiddenClassName}`).forEach(el => {
       el.style.visibility = '';
       el.classList.remove(hiddenClassName);
     });
-    return;
   }
-
-  function applyHidingRules() {
-    selectorsToHide.forEach(selector => {
-      document.querySelectorAll(selector).forEach(element => {
-        if (!element.classList.contains(hiddenClassName)) {
-          element.style.visibility = 'hidden';
-          element.classList.add(hiddenClassName);
-        }
-      });
-    });
-  }
-
-  applyHidingRules();
-
-  const observer = new MutationObserver(mutations => {
-    applyHidingRules();
-  });
-
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true
-  });
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === "toggleSpoilers") {
-    hideSpoilers(request.enabled);
+    toggleSpoilers(request.enabled);
   }
 });
 
 chrome.storage.sync.get('spoilerEnabled', function(data) {
   if (data.spoilerEnabled !== false) {
-    hideSpoilers(true);
+    toggleSpoilers(true);
   }
 });
